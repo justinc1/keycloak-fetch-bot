a4bd01148b6a7951a7e596cb4b74686af1356118
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-08-12 08:37:03 +0200

    CI load environ from env.env
    env.sh has same content, is used if pytest is run outside of tox
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,5 +1,7 @@
 import json
 import os
+from copy import copy
+
 from kcfetcher.utils import remove_folder
 from kcfetcher.main import run
 
@@ -7,6 +9,26 @@ from kcfetcher.main import run
 # Does main produce expected result.
 # A new subdir is created for master realm, and it must contain some json files etc.
 class TestMain:
+    _env_file = "env.env"
+    _os_environ_before = {}
+
+    @classmethod
+    def setup_class(cls):
+        env_lines = open("env.env").readlines()
+        env_lines = [el for el in env_lines if el.strip()]
+        cls._os_environ_before = copy(os.environ)
+        for line in env_lines:
+            kk, vv = line.strip().split('=', 1)
+            os.environ[kk] = vv
+
+    @classmethod
+    def teardown_class(cls):
+        env_lines = open("env.env").readlines()
+        env_lines = [el for el in env_lines if el.strip()]
+        for line in env_lines:
+            kk, vv = line.strip().split('=', 1)
+            os.environ[kk] = cls._os_environ_before[kk]
+
     @staticmethod
     def test_main():
         output_dir = './output/ci/outd/'
