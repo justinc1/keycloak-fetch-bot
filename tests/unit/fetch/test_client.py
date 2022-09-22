9a7202c6bfb5fddc6c27686b2185660b1ace3a67
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-09-22 10:28:54 +0200

    Test ClientFetch with VCR instead of mockup
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- tests/unit/fetch/test_client.py
+++ tests/unit/fetch/test_client.py
@@ -3,6 +3,8 @@ import json
 import os
 import shutil
 from kcfetcher.fetch import ClientFetch
+from kcfetcher.store import Store
+from kcfetcher.utils import remove_folder, make_folder, login
 
 
 class MockupStoreApi:
@@ -37,10 +39,80 @@ class TestClientFetch:
     def test_fetch(self):
         store_api = MockupStoreApi()
         kc = MockupKc()
-        resource_name = "ci-resource"
+        resource_name = "clients"
 
         obj = ClientFetch(kc, resource_name)
-        obj.id = "ci-id"
+        obj.id = "clientId"
         obj.realm = "ci-realm"
 
         obj.fetch(store_api)
+
+
+@mark.vcr()
+class TestClientFetch_vcr:
+    def test_fetch(self):
+        datadir = "output/ci/outd"
+        remove_folder(datadir)
+        make_folder(datadir)
+        store_api = Store(datadir)
+        server = os.environ["SSO_API_URL"]
+        user = os.environ["SSO_API_USERNAME"]
+        password = os.environ["SSO_API_PASSWORD"]
+        kc = login(server, user, password)
+        realm_name = "master"
+        resource_name = "clients"
+        resource_identifier = "clientId"
+
+        obj = ClientFetch(kc, resource_name, resource_identifier, realm_name)
+
+        obj.fetch(store_api)
+
+        # check generated content
+        assert os.listdir(datadir) == ["client-0"]
+        assert os.listdir(os.path.join(datadir, "client-0")) == ['master-realm.json', 'roles']
+        assert os.listdir(os.path.join(datadir, "client-0/roles")) == ['roles.json']
+        #
+        data = json.load(open(os.path.join(datadir, "client-0/master-realm.json")))
+        assert list(data.keys()) == [
+            'access',
+            'alwaysDisplayInConsole',
+            'attributes',
+            'authenticationFlowBindingOverrides',
+            'bearerOnly',
+            'clientAuthenticatorType',
+            'clientId',
+            'consentRequired',
+            'defaultClientScopes',
+            'directAccessGrantsEnabled',
+            'enabled',
+            'frontchannelLogout',
+            'fullScopeAllowed',
+            'implicitFlowEnabled',
+            'name',
+            'nodeReRegistrationTimeout',
+            'notBefore',
+            'optionalClientScopes',
+            'publicClient',
+            'redirectUris',
+            'serviceAccountsEnabled',
+            'standardFlowEnabled',
+            'surrogateAuthRequired',
+            'webOrigins',
+        ]
+        assert data["clientId"] == "master-realm"
+        assert data["name"] == "master Realm"
+        assert os.listdir(os.path.join(datadir, "client-0/roles")) == ['roles.json']
+        #
+        data = json.load(open(os.path.join(datadir, "client-0/roles/roles.json")))
+        assert isinstance(data, list)
+        assert len(data) == 18
+        role = data[0]
+        assert list(role.keys()) == [
+            'clientRole',
+            'composite',
+            'containerId',
+            'description',
+            'name',
+        ]
+        assert role["name"] == "view-authorization"
+        assert role["description"] == "${role_view-authorization}"
