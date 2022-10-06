259cb0a34f2a3b243b6401d5fe12e3c1c4580df8
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-10-06 22:17:41 +0200

    Include group names for each user
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- kcfetcher/fetch/__init__.py
+++ kcfetcher/fetch/__init__.py
@@ -1,11 +1,13 @@
 from .generic import GenericFetch
 from .client import ClientFetch
 from .custom_authentication import CustomAuthenticationFetch
+from .user import UserFetch
 from .factory import FetchFactory
 
 __all__ = [
     GenericFetch,
     ClientFetch,
     CustomAuthenticationFetch,
+    UserFetch,
     FetchFactory,
 ]
