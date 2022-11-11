bc90c358fd00e632ae1b158384f9fc0396d3d290
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-11-11 12:42:18 +0100

    Remove old testing code
    We want to use vcr testing, not mockups.
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- tests/unit/fetch/test_client.py
+++ tests/unit/fetch/test_client.py
@@ -2,51 +2,9 @@ from pytest import mark
 from pytest_unordered import unordered
 import json
 import os
-import shutil
 from kcfetcher.fetch import ClientFetch
 from kcfetcher.store import Store
 from kcfetcher.utils import remove_folder, make_folder, login
-
-
-class MockupStoreApi:
-    def add_child(self, child_name):
-        pass
-
-    def store_one(self, data, identifier):
-        pass
-
-    def store_one_with_alias(self, alias, data):
-        pass
-
-    def remove_last_child(self):
-        pass
-
-
-class MockupKc:
-    def build(self, name, realm):
-        return MockupClients()
-
-
-class MockupClients:
-    def roles(self, query):
-        return {}
-
-    def all(self):
-        return []
-
-
-# manually written mockup classes
-class TestClientFetch:
-    def test_fetch(self):
-        store_api = MockupStoreApi()
-        kc = MockupKc()
-        resource_name = "clients"
-
-        obj = ClientFetch(kc, resource_name)
-        obj.id = "clientId"
-        obj.realm = "ci-realm"
-
-        obj.fetch(store_api)
 
 
 @mark.vcr()
