6a87fec2823757e614190aa849af6c12e124da0b
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-11-15 17:59:36 +0100

    Reuse RoleFetch to fetch both realm and client roles
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- kcfetcher/fetch/client.py
+++ kcfetcher/fetch/client.py
@@ -1,6 +1,7 @@
 from kcapi.rest.crud import KeycloakCRUD
 
 from kcfetcher.fetch import GenericFetch
+from kcfetcher.fetch.role import RoleFetch
 from kcfetcher.utils import find_in_list, minimize_role_representation
 
 
@@ -24,6 +25,7 @@ class ClientFetch(GenericFetch):
         counter = 0
         client_id_all = [client["id"] for client in kc_objects]
         for kc_object in kc_objects:
+            client_id = kc_object['id']
             store_api.add_child('client-' + str(counter))  # clients/<client_ind>
             # authenticationFlowBindingOverrides need to be saved with auth flow alias/name, not id/UUID
             for auth_flow_override in kc_object["authenticationFlowBindingOverrides"]:
@@ -32,44 +34,9 @@ class ClientFetch(GenericFetch):
                 kc_object["authenticationFlowBindingOverrides"][auth_flow_override] = auth_flow_alias
             store_api.store_one(kc_object, identifier)
 
-            client_query = {'key': 'clientId', 'value': kc_object['clientId']}
-            # GET /{realm}/clients/{id}/roles - briefRepresentation=True is default
-            # We get full RoleRepresentation from
-            #   GET /{realm}/clients/{id}/roles/{role-name} or
-            #   GET /{realm}/roles-by-id/{role-id}
-            # RoleRepresentation includes .attributes attribute.
-            roles_brief = clients_api.roles(client_query).all()
-            roles_by_id_api = self.kc.build("roles-by-id", realm)
-            roles = [
-                roles_by_id_api.get(role_brief['id']).verify().resp().json()
-                for role_brief in roles_brief
-            ]
-            # But composites are missing :/.
-            # Get them from GET /{realm}/clients/{id}/roles/{role-name}/composites.
-            client_id = kc_object['id']
-            client_roles_api = self.kc.build(f"clients/{client_id}/roles", realm)
-            for role in roles:
-                # the containerId needs to be removed, it is client clientID (UUID)
-                assert role["containerId"] == client_id
-                role.pop("containerId")
-
-                if not role["composite"]:
-                    continue
-                composites = client_roles_api.get(f"{role['name']}/composites").verify().resp().json()
-
-                if 0:
-                    # Those two are same. Are they nicer the code above?
-                    client_role_composites_api = clients_api.roles(client_query).get_child(clients_api, f"{client_id}/roles/{role['name']}", "composites")
-                    client_role_composites_api = KeycloakCRUD.get_child(clients_api, f"{client_id}/roles/{role['name']}", "composites")
-                    x = client_role_composites_api.get(_id=None).verify().resp().json()
-
-                # Now add composites into role dict
-                # For client role, we need to replace containerId (UUID) with client.clientId (string)
-                assert "composites" not in role
-                role["composites"] = [minimize_role_representation(cc, kc_objects) for cc in composites]
-
+            role_fetcher = RoleFetch(self.kc, f"clients/{client_id}/roles", "name", self.realm)
             store_api.add_child('roles')  # clients/<client_ind>/roles
-            store_api.store(roles, "name")
+            role_fetcher.fetch(store_api)
             store_api.remove_last_child()  # clients/<client_ind>/roles
 
             # Compute scope-mappings
--- kcfetcher/fetch/role.py
+++ kcfetcher/fetch/role.py
@@ -10,8 +10,18 @@ logger = logging.getLogger(__name__)
 class RoleFetch(GenericFetch):
     def __init__(self, kc, resource_name, resource_id="", realm=""):
         super().__init__(kc, resource_name, resource_id, realm)
-        assert "roles" == self.resource_name
         assert "name" == self.id
+
+        # assert self.resource_name in ["roles", "clients/1e0807cf-7aa1-4ddf-8aa0-7d117d476785/roles"]
+        if self.resource_name == "roles":
+            # fetching realm roles
+            pass
+        else:
+            # fetching client roles, client by client
+            assert len(self.resource_name) == 50
+            assert len(self.resource_name.split("/")) == 3
+            assert self.resource_name.split("/")[0] == "clients"
+            assert self.resource_name.split("/")[2] == "roles"
 
     def _get_data(self):
         roles_api = self.kc.build(self.resource_name, self.realm)
@@ -27,7 +37,11 @@ class RoleFetch(GenericFetch):
             role = roles_by_id_api.get(role_id).verify().resp().json()
 
             # the containerId needs to be removed, it is UUID (clientID for client roles, realm name for realm roles)
-            assert role["containerId"] == self.realm
+            if role['clientRole']:
+                # must be parent client UUID, check the length
+                assert len(role["containerId"]) == 36
+            else:
+                assert role["containerId"] == self.realm
             role.pop("containerId")
 
             if role["composite"]:
