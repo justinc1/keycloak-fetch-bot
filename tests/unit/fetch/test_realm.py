d7d21fcc4141de64c18e4d2b5a7b24ae818877e1
Justin Cinkelj <justin.cinkelj@xlab.si>
2022-11-15 20:16:12 +0100

    CI update test to use RealmFetcher
    Test also master.json
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- tests/unit/fetch/test_realm.py
+++ tests/unit/fetch/test_realm.py
@@ -3,7 +3,7 @@ from pytest_unordered import unordered
 import json
 import os
 from path import glob
-# from kcfetcher.fetch import GenericFetch
+from kcfetcher.fetch import RealmFetch
 from kcfetcher.store import Store
 from kcfetcher.utils import remove_folder, make_folder, login
 
@@ -22,11 +22,19 @@ class TestRealmFetch_vcr:
 
         # Do what main.py does.
         realms_api = kc.admin()
+        # for realm in realms_api.all():
+        #     current_realm = realm['realm']
+        #     store = Store(path=datadir)
+        #     store.add_child(current_realm)
+        #     store.store_one(realm, 'realm')
+
+        store = Store(path=datadir)
+        realm_fetcher = RealmFetch(kc)
         for realm in realms_api.all():
-            current_realm = realm['realm']
-            store = Store(path=datadir)
-            store.add_child(current_realm)
-            store.store_one(realm, 'realm')
+            realm_name = realm['realm']
+            store.add_child(realm_name)  # outd/<realm>
+            realm_fetcher.fetch_one(store, realm)
+            store.remove_last_child()  # outd/<realm>
 
         # check generated content
         assert unordered(glob.glob('**', root_dir=datadir, recursive=True)) == [
@@ -140,3 +148,101 @@ class TestRealmFetch_vcr:
         assert data["internationalizationEnabled"] == False
         assert data["clientAuthenticationFlow"] == "clients"
 
+        data = json.load(open(os.path.join(datadir, "master/master.json")))
+        assert list(data.keys()) == [
+            'accessCodeLifespan',
+            'accessCodeLifespanLogin',
+            'accessCodeLifespanUserAction',
+            'accessTokenLifespan',
+            'accessTokenLifespanForImplicitFlow',
+            'actionTokenGeneratedByAdminLifespan',
+            'actionTokenGeneratedByUserLifespan',
+            'adminEventsDetailsEnabled',
+            'adminEventsEnabled',
+            'attributes',
+            'browserFlow',
+            'browserSecurityHeaders',
+            'bruteForceProtected',
+            'clientAuthenticationFlow',
+            'clientOfflineSessionIdleTimeout',
+            'clientOfflineSessionMaxLifespan',
+            'clientPolicies',
+            'clientProfiles',
+            'clientSessionIdleTimeout',
+            'clientSessionMaxLifespan',
+            'defaultRole',
+            'defaultSignatureAlgorithm',
+            'directGrantFlow',
+            'displayName',
+            'displayNameHtml',
+            'dockerAuthenticationFlow',
+            'duplicateEmailsAllowed',
+            'editUsernameAllowed',
+            'enabled',
+            'enabledEventTypes',
+            'eventsEnabled',
+            'eventsListeners',
+            'failureFactor',
+            'identityProviderMappers',
+            'identityProviders',
+            'internationalizationEnabled',
+            'loginWithEmailAllowed',
+            'maxDeltaTimeSeconds',
+            'maxFailureWaitSeconds',
+            'minimumQuickLoginWaitSeconds',
+            'notBefore',
+            'oauth2DeviceCodeLifespan',
+            'oauth2DevicePollingInterval',
+            'offlineSessionIdleTimeout',
+            'offlineSessionMaxLifespan',
+            'offlineSessionMaxLifespanEnabled',
+            'otpPolicyAlgorithm',
+            'otpPolicyDigits',
+            'otpPolicyInitialCounter',
+            'otpPolicyLookAheadWindow',
+            'otpPolicyPeriod',
+            'otpPolicyType',
+            'otpSupportedApplications',
+            'permanentLockout',
+            'quickLoginCheckMilliSeconds',
+            'realm',
+            'refreshTokenMaxReuse',
+            'registrationAllowed',
+            'registrationEmailAsUsername',
+            'registrationFlow',
+            'rememberMe',
+            'requiredCredentials',
+            'resetCredentialsFlow',
+            'resetPasswordAllowed',
+            'revokeRefreshToken',
+            'smtpServer',
+            'sslRequired',
+            'ssoSessionIdleTimeout',
+            'ssoSessionIdleTimeoutRememberMe',
+            'ssoSessionMaxLifespan',
+            'ssoSessionMaxLifespanRememberMe',
+            'supportedLocales',
+            'userManagedAccessAllowed',
+            'verifyEmail',
+            'waitIncrementSeconds',
+            'webAuthnPolicyAcceptableAaguids',
+            'webAuthnPolicyAttestationConveyancePreference',
+            'webAuthnPolicyAuthenticatorAttachment',
+            'webAuthnPolicyAvoidSameAuthenticatorRegister',
+            'webAuthnPolicyCreateTimeout',
+            'webAuthnPolicyPasswordlessAcceptableAaguids',
+            'webAuthnPolicyPasswordlessAttestationConveyancePreference',
+            'webAuthnPolicyPasswordlessAuthenticatorAttachment',
+            'webAuthnPolicyPasswordlessAvoidSameAuthenticatorRegister',
+            'webAuthnPolicyPasswordlessCreateTimeout',
+            'webAuthnPolicyPasswordlessRequireResidentKey',
+            'webAuthnPolicyPasswordlessRpEntityName',
+            'webAuthnPolicyPasswordlessRpId',
+            'webAuthnPolicyPasswordlessSignatureAlgorithms',
+            'webAuthnPolicyPasswordlessUserVerificationRequirement',
+            'webAuthnPolicyRequireResidentKey',
+            'webAuthnPolicyRpEntityName',
+            'webAuthnPolicyRpId',
+            'webAuthnPolicySignatureAlgorithms',
+            'webAuthnPolicyUserVerificationRequirement',
+        ]
